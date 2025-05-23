bd7994b412aeaa73ad7093cb46a52a2940159484
Kayk_Darios <manjac688@gmail.com>
2025-05-23 02:28:57 -0300

    feat: criando pagina/rota destinda a usuarioLogadoNormal

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,15 @@
+
+
+const Dashboard = () => {
+
+
+    return (
+      <div className="p-10">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <p>Bem-vindo ao seu painel.</p>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
