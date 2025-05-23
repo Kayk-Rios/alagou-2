4434f9b8e85d433c65669a33e33d40f68370ba04
Kayk_Darios <manjac688@gmail.com>
2025-05-23 02:27:46 -0300

    feat: utiliziando o header e footer no componente app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+   
+   <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+    </>
+  );
 }
