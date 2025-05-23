705f9a84d417b04fa3ca73b50860730750c010f3
Kayk_Darios <manjac688@gmail.com>
2025-05-23 02:28:30 -0300

    feat: criando pagina/rota destinda a admin

---
+++ src/pages/admin/admin.tsx
@@ -0,0 +1,16 @@
+
+const AdminPage = () => {
+
+
+
+
+    return (
+      <div className="p-10">
+        <h1 className="text-3xl font-bold">Área do Administrador</h1>
+        <p>Bem-vindo à área restrita para administradores.</p>
+      </div>
+    );
+  };
+  
+  export default AdminPage;
+  
