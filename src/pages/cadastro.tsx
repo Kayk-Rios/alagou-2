955a2e6aafa181f6519802bb3f1ae0b249b008d7
Kayk_Darios <manjac688@gmail.com>
2025-05-23 02:32:37 -0300

    feat: exibiçao d elogin e cadastro

---
+++ src/pages/cadastro.tsx
@@ -0,0 +1,12 @@
+import Cadastro from "@/components/Cadastro";
+
+
+const CadastroPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Cadastro />
+    </div>
+  );
+};
+
+export default CadastroPage;
