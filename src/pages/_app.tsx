9ff5648e3e94892dc0bc754538b2ec51f477b981
Kayk_Darios <manjac688@gmail.com>
2025-05-25 01:16:07 -0300

    chore: importaçao de leaflet para usar o css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
+import 'leaflet/dist/leaflet.css'
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
